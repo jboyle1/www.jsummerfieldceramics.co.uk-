7bbf900c8dbe9d89e3c33cd90812a514410f776d
Jed Boyle <jed-boyle@Jeds-iMac.local>
2021-01-03 16:43:50 +0000

    Add extra SEO meta tags.

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,17 +1,18 @@
 function sendMail(contactForm) {
-  emailjs
-    .send("gmail", "template_avBcvR14", {
-      from_name: contactForm.name.value,
-      from_email: contactForm.emailaddress.value,
-      commission_request: contactForm.projectsummary.value
-    })
-    .then(
-      function(response) {
-        console.log("SUCCESS", response);
-      },
-      function(error) {
-        console.log("FAILED", error);
-      }
-    );
-  return false; // To block from loading a new page
+    emailjs
+        .send("gmail", "template_avBcvR14", {
+            from_name: contactForm.name.value,
+            from_email: contactForm.emailaddress.value,
+            commission_request: contactForm.projectsummary.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+                window.open("thank-you-page.html")
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
 }
